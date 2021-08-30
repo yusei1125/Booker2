fd211adc9ae85d559a1c3e474b1325294b862526
yusei1125 <sk11250925@gmail.com>
2021-08-30 10:24:48 +0000

    refile修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a5a7e34198f1600c12c955f4b63240f972d7d397afe26f91e246c1fdb5fb2512edf09db4f2cd179722fb21f0102a738ad9ca4dfabdece8739bcbbb5dea78aec2'
